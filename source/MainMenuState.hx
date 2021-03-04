3ad4a1d7d143f932ca4f68a8b3349e81b56a28f3
Radriant <44790637+Radriant@users.noreply.github.com>
2021-03-04 15:51:09 -0600

    Update MainMenuState.hx
    idk cont

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -30,6 +30,8 @@ class MainMenuState extends MusicBeatState
 
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
+	
+	var trackedAssets:Array<Dynamic> = [];
 
 	override function create()
 	{
@@ -162,6 +164,7 @@ class MainMenuState extends MusicBeatState
 							FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
 							{
 								var daChoice:String = optionShit[curSelected];
+								unloadAssets();
 
 								switch (daChoice)
 								{
@@ -214,5 +217,19 @@ class MainMenuState extends MusicBeatState
 
 			spr.updateHitbox();
 		});
+	}
+								
+	override function add(Object:flixel.FlxBasic):flixel.FlxBasic
+	{
+		trackedAssets.insert(trackedAssets.length, Object);
+		return super.add(Object);
+	}
+
+	function unloadAssets():Void
+	{
+		for (asset in trackedAssets)
+		{
+			remove(asset);
+		}
 	}
 }
